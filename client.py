9fc75099b6a70f50caa3070c5f916b7f8b001bb1
EarthmanAman <hashimathman@gmail.com>
2020-11-20 22:47:15 +0300

    done

--- client.py
+++ client.py
@@ -1,11 +1,47 @@
+# ATHMAN HASHIM ABDALLA
+# P15 / 81781 / 2017
+
+
 from multiprocessing.connection import Client
+"""
+Purpose
+--------
+	* Creating a client which will be using ssl to communicate with server
 
+"""
+
+# Creating client
 address = ('localhost', 8000)
-conn = Client(address, authkey=b'secret password')
+conn = Client(address)
 
-# can also send arbitrary objects:
-while True:
-	msg = input("msg >> ")
-	conn.send(msg)
-# conn.send(['a', 2.5, None, int, sum])
-conn.close()
+# Initializing handshake
+
+print("Sending Request to server...\n")
+conn.send("client_hello")
+
+
+# Receiing server response
+rep = conn.recv()
+print("Receiving server acceptance... \n")
+server_pk = conn.recv()
+k = 2
+
+# Calculating pre master using the server public key
+pre_master_key = server_pk * k
+print("Sending pre master key ... \n")
+msg1 = "sending pre_master_key encoded text {}".format(pre_master_key)
+conn.send(msg1)
+
+# Receiving master key
+recv_master = conn.recv()
+print("Receiving master key ... \n")
+received_master = recv_master.split()
+master_key = int(received_master[-1]) / k
+
+print("Master key", master_key)
+
+# Sending hello world
+hello = "Hello world {}".format(master_key)
+conn.send(hello)
+
+conn.close()	
--- server.py
+++ server.py
@@ -1,13 +1,74 @@
+# ATHMAN HASHIM ABDALLA
+# P15 / 81781 / 2017
+
 from multiprocessing.connection import Listener
 
+"""
+Purpose
+--------
+	* To create a server to accept ssl
+
+Method of encryption
+----------------------
+	* I used a simple encryption techniques just for demostration 
+	* The master key is appended at the end of each string.
+"""
+
+# Creating a server process
+print("Server listening ... \n")
 address = ('localhost', 8000)     # family is deduced to be 'AF_INET'
-listener = Listener(address, authkey=b'secret password')
+listener = Listener(address)
 conn = listener.accept()
+
+# The keys of the server
+pk = 3
+secret_key = 24 / pk
+
 print('connection accepted from', listener.last_accepted)
-while True:
-	msg = conn.recv()
-	print(f"Received: {msg}")
-	if msg == 'close':
-		conn.close()
-		break
+
+# Receiving the first message which is "client_hello"
+msg = conn.recv()
+print("Receiving client request... \n")
+
+# Check if it the client_hello i.e. a request by client to connect
+if msg == "client_hello":
+	conn.send("server_hello")
+	conn.send(pk) # Send public key to client
+	print("Sending Server Public Key... \n")
+
+# Receiving Pre master encrypted message from client	
+pre_master_encrypt_msg = conn.recv()
+print("Receiving pre master key")
+split_msg = pre_master_encrypt_msg.split()
+k = round(int(split_msg[-1]) / secret_key * pk) # extract the pre master key
+print("Pre master key is: " + str(k))
+
+# I used a constant figure 8 as the master key for simplicity
+master_key = 8
+
+# Encrypting the master key with the pre master key 
+encryt_master_key_value = k * master_key
+
+# Send the encrypted master key
+print("\nSending master key")
+conn.send("master {}".format(encryt_master_key_value))
+
+
+print("\n\nHandshake complete \n\n")
+
+# Start receiving 
+
+	
+msg_recv = conn.recv()
+msg_split = msg_recv.split()
+
+if float(msg_split[-1]) == float(master_key):
+	print("Received >> {} {}".format(msg_split[0], msg_split[1]))
+	
+else:
+	print("Corrupted data. Initiate the processes again")
+	conn.close()
+
+conn.close()
+
 listener.close()
